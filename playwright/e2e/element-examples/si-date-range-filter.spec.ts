fcd84626116b0c32f4550d7c28b53e82cbc80b93
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-06-27 10:40:52 +0200

    feat(date-range-filter): improve layout for smaller screens

---
+++ playwright/e2e/element-examples/si-date-range-filter.spec.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright Siemens 2016 - 2025.
+ * SPDX-License-Identifier: MIT
+ */
+import { expect, test } from '../../support/test-helpers';
+
+test.describe('si-date-range-filter', () => {
+  test.beforeEach(async ({ page }) => {
+    // Ensure the current date and time for running the tests is always the same
+    await page.clock.setFixedTime(Date.UTC(2023, 7, 8, 10, 34, 24, 345));
+  });
+
+  const example = 'si-date-range-filter/si-date-range-filter';
+
+  test(example, async ({ page, si }) => {
+    await si.visitExample(example);
+    await si.runVisualAndA11yTests('default');
+  });
+
+  test(example + ' to date', async ({ page, si }) => {
+    await si.visitExample(example);
+
+    await page.getByText('Input mode').click();
+    await expect(page.locator('input[name=point2]')).toHaveCount(1);
+
+    await si.runVisualAndA11yTests('date');
+  });
+
+  test(example + ' range', async ({ page, si }) => {
+    await si.visitExample(example);
+
+    await page.locator('si-date-range-filter').getByText('Advanced').click();
+    await expect(page.getByText('Before')).toHaveCount(1);
+
+    await si.runVisualAndA11yTests('range');
+  });
+
+  test(example + ' mobile', async ({ page, si }) => {
+    await page.setViewportSize({ width: 400, height: 660 });
+    await si.visitExample(example);
+    await si.runVisualAndA11yTests('mobile');
+
+    await page.locator('si-date-range-filter').getByText('Presets').click();
+    await si.runVisualAndA11yTests('mobile-presets');
+  });
+});
