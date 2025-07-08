ddfc23a28ce941a9d4e128949703a548a7bf4c77
Chintan Kavathia <chintankumar.kavathia@siemens.com>
2025-07-08 11:19:32 +0530

    test(card): add e2e for selectable cards

---
+++ playwright/e2e/element-examples/si-card-selectable.spec.ts
@@ -0,0 +1,36 @@
+/**
+ * Copyright Siemens 2016 - 2025.
+ * SPDX-License-Identifier: MIT
+ */
+import { expect, test } from '../../support/test-helpers';
+
+test.describe('si-card-selectable', () => {
+  const example = 'si-card/si-card-selectable';
+
+  test(example, async ({ page, si }) => {
+    await si.visitExample(example, false);
+
+    const card = page.getByRole('button', {
+      name: 'FS20'
+    });
+
+    await expect(card).toBeVisible();
+
+    await card.click();
+
+    // this moves cursor hover effect off the card
+    await page.click('body', { position: { x: 0, y: 0 } });
+
+    await expect(card).toHaveAttribute('aria-pressed', 'true');
+
+    await si.runVisualAndA11yTests();
+
+    const anotherCard = page.getByRole('button', {
+      name: 'Natural Gas Usage'
+    });
+
+    await anotherCard.hover();
+
+    await si.runVisualAndA11yTests('hover');
+  });
+});
