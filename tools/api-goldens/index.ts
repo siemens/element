eb2794b02b40881eb6e073a8f725f6131460e0f3
Maximilian Koeller <maximilian.koeller@siemens.com>
2025-07-09 17:35:45 +0200

    build(api): introduce api goldens

---
+++ tools/api-goldens/index.ts
@@ -0,0 +1,43 @@
+/**
+ * Copyright Siemens 2016 - 2025.
+ * SPDX-License-Identifier: MIT
+ */
+/* eslint-disable no-console */
+import * as path from 'path';
+
+import { main } from './index_npm_packages';
+
+(async () => {
+  const approveGolden = process.argv[2] === 'accept';
+
+  const packages = [
+    'charts-ng',
+    'element-ng',
+    'element-translate-ng',
+    'live-preview',
+    'native-charts-ng'
+  ];
+
+  const outdatedGoldens: string[] = [];
+  for (const packageName of packages) {
+    console.log('Extracting API for package:', packageName);
+    const outdated = await main(
+      path.resolve(`api-goldens/${packageName}`),
+      path.resolve(`dist/@siemens/${packageName}`),
+      approveGolden,
+      /ɵ/,
+      []
+    );
+    outdatedGoldens.push(...outdated);
+  }
+
+  if (outdatedGoldens.length) {
+    console.error(`The following goldens are outdated:`);
+    outdatedGoldens.forEach(name => console.info(`-  ${name}`));
+    console.info();
+    console.info(
+      `The goldens can be updated by running: "rm -rf .angular && npm run build:all && npm run api-goldens:accept"`
+    );
+    process.exitCode = 1;
+  }
+})();
