b0238c3bdd2a810a1636337f427720ae4c7abfca
Marco D'Auria <marco.dauria@siemens.com>
2025-07-18 13:42:53 +0200

    fix(live-preview): optimize routes navigation and state management

--- src/app/examples/si-navbar-vertical/si-navbar-vertical-routing.ts
+++ src/app/examples/si-navbar-vertical/si-navbar-vertical-routing.ts
@@ -2,8 +2,8 @@
  * Copyright (c) Siemens 2016 - 2025
  * SPDX-License-Identifier: MIT
  */
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Route, RouterLink, RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import {
   SiApplicationHeaderComponent,
   SiHeaderBrandDirective,
@@ -12,7 +12,7 @@ import {
 import { SiBreadcrumbRouterComponent } from '@siemens/element-ng/breadcrumb-router';
 import { Link, SiLinkDirective } from '@siemens/element-ng/link';
 import { NavbarVerticalItem, SiNavbarVerticalComponent } from '@siemens/element-ng/navbar-vertical';
-import { provideExampleRoutes } from '@siemens/live-preview';
+import { EXAMPLE_ROUTE_MAP } from '@siemens/live-preview';
 
 // Dummy components to be used in the router outlet for the example
 @Component({
@@ -41,8 +41,8 @@ export class EnergyComponent {}
     <router-outlet /> `
 })
 export class TestCoverageComponent {
-  e2eRouterLink: Link = { link: 'e2e-coverage' };
-  unitRouterLink: Link = { link: 'unit-coverage' };
+  e2eRouterLink: Link = { link: 'child1' };
+  unitRouterLink: Link = { link: 'child2' };
 }
 
 @Component({
@@ -57,24 +57,13 @@ export class E2ECoverageComponent {}
 })
 export class UnitCoverageComponent {}
 
-export const ROUTES: Route[] = [
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  { path: 'energy', component: EnergyComponent },
-  {
-    path: 'coverage',
-    component: TestCoverageComponent,
-    children: [
-      { path: 'e2e-coverage', component: E2ECoverageComponent },
-      {
-        path: 'unit-coverage',
-        component: UnitCoverageComponent
-      }
-    ]
-  }
-];
+export const exampleRouteMap = {
+  route1: HomeComponent,
+  route2: EnergyComponent,
+  route3: TestCoverageComponent,
+  'route3/child1': E2ECoverageComponent,
+  'route3/child2': UnitCoverageComponent
+};
 
 @Component({
   selector: 'app-sample',
@@ -89,31 +78,36 @@ export const ROUTES: Route[] = [
   ],
   templateUrl: './si-navbar-vertical-routing.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [provideExampleRoutes(ROUTES)]
+  providers: [{ provide: EXAMPLE_ROUTE_MAP, useValue: exampleRouteMap }]
 })
-export class SampleComponent {
+export class SampleComponent implements OnInit {
+  private router = inject(Router);
   menuItems: NavbarVerticalItem[] = [
     {
       type: 'router-link',
       label: 'Home',
       id: 'home',
       icon: 'element-home',
-      routerLink: 'home'
+      routerLink: 'route1'
     },
     { type: 'header', label: 'Modules' },
     {
       type: 'router-link',
       label: 'Energy & sustainability',
       icon: 'element-trend',
-      routerLink: 'energy'
+      routerLink: 'route2'
     },
     {
       type: 'router-link',
       label: 'Test coverage',
       icon: 'element-diagnostic',
-      routerLink: 'coverage',
+      routerLink: 'route3',
       badge: 4,
       badgeColor: 'danger'
     }
   ];
+
+  ngOnInit(): void {
+    this.router.navigate(['iframe/route1']);
+  }
 }
