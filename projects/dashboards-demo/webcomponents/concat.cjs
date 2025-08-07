1fdb22f31596be60c730f01c2bcec5e3d0d38f73
Chintan Kavathia <chintankumar.kavathia@siemens.com>
2025-08-07 11:17:23 +0530

    chore(dashboards): enable repeated dashboards webcomponent build

--- projects/dashboards-demo/webcomponents/concat.cjs
+++ projects/dashboards-demo/webcomponents/concat.cjs
@@ -6,7 +6,11 @@ build = async () => {
     './dist/dashboards-demo-webcomponents/runtime.js',
     './dist/dashboards-demo-webcomponents/main.js'
   ];
-  fs.mkdirSync('./dist/dashboards-demo/webcomponents/');
+  const dir = './dist/dashboards-demo/webcomponents/';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
   await concat(files, 'dist/dashboards-demo/webcomponents/webcomponent-widgets.js');
+  await concat(files, 'dist/dashboards-demo-webcomponents/webcomponent-widgets.js');
 };
 build();
