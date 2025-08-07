b560102e7df0179bc1da99bac929194493705098
Loris Janner <loris.janner@siemens.com>
2025-08-07 18:06:26 +0200

    chore(docs): add micro charts to API docs

---
+++ projects/native-charts-ng/docs.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+
+export * from './micro-bar';
+export * from './micro-donut';
+export * from './micro-line';
+export * from './micro-progress';
