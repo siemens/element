382d6dff1abec94ff73b2047ff1366f85eccf30b
Marco D'Auria <marco.dauria@siemens.com>
2025-08-19 06:21:39 +0200

    feat(buttons): introduce button group with sub variants

---
+++ src/app/examples/buttons/button-groups.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  imports: [],
+  templateUrl: './button-groups.html'
+})
+export class SampleComponent {}
