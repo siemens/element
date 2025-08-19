4ed3d29226dc7dd302b0b4dec1b998e61d234ddd
Marco D'Auria <marco.dauria@siemens.com>
2025-08-19 06:21:39 +0200

    feat(buttons): add split button component

--- src/app/examples/buttons/buttontypes.ts
+++ src/app/examples/buttons/buttontypes.ts
@@ -2,10 +2,13 @@
  * Copyright (c) Siemens 2016 - 2025
  * SPDX-License-Identifier: MIT
  */
+import { CdkMenuTrigger } from '@angular/cdk/menu';
 import { Component } from '@angular/core';
+import { SiMenuModule } from '@siemens/element-ng/menu';
 
 @Component({
   selector: 'app-sample',
+  imports: [SiMenuModule, CdkMenuTrigger],
   templateUrl: './buttontypes.html',
   host: {
     class: 'bg-base-1'
@@ -13,4 +16,16 @@ import { Component } from '@angular/core';
 })
 export class SampleComponent {
   disabled = false;
+  splitOpen1 = false;
+  splitOpen2 = false;
+  splitOpen3 = false;
+  splitOpen4 = false;
+  splitOpen5 = false;
+  splitOpen6 = false;
+
+  menuItems = [
+    { icon: 'element-download', text: 'Download as PDF' },
+    { icon: 'element-download', text: 'Download as CSV' },
+    { icon: 'element-share', text: 'Share' }
+  ];
 }
