5eb92c0933080413dcacd547591f523401e47831
chintankavathia <chintankumar.kavathia@siemens.com>
2025-09-02 15:24:17 +0530

    refactor(tree-view): use siTreeViewItem for custom templates
    DEPRECATED: `siTreeViewItemTemplate` is deprecated in favor of `siTreeViewItem`.
    
    Currently there are 2 directives `siTreeViewItemTemplate` for providing custom template and `siTreeViewItem` for applying custom directives (e.g cdkDrag) on tree item.
    
    the `siTreeViewItemTemplate` has 2 main DX problems:
    
    - Every tree item needs to specify templateName in its json structure.
    - For every unique custom template there needs a separate ng-template with exact mapping of that templateName which doesn't provide type safety.
    
    Migration Guide:
    
    Before (deprecated):
    ```html
    <si-tree-view>
      <ng-template siTreeViewItemTemplate="root" let-item>
        <div class="custom-item">Root {{ item.name }}</div>
      </ng-template>
      <ng-template siTreeViewItemTemplate="child" let-item>
        <div class="custom-item">Child {{ item.name }}</div>
      </ng-template>
    </si-tree-view>
    ```
    
    After (recommended):
    ```html
    <si-tree-view>
      <ng-template siTreeViewItem let-item="treeItem">
        <si-tree-view-item>
          <div class="custom-item">
          @if (item.level === 0) {
            Root {{ item.name }}
          } @else {
            Child {{ item.name }}
          }
          </div>
        </si-tree-view-item>
      </ng-template>
    </si-tree-view>
    ```

--- projects/element-ng/tree-view/si-tree-view-item-template.directive.ts
+++ projects/element-ng/tree-view/si-tree-view-item-template.directive.ts
@@ -6,6 +6,45 @@ import { Directive, inject, input, TemplateRef } from '@angular/core';
 
 import { TreeItem } from './si-tree-view.model';
 
+/**
+ * Tree view item template directive for defining custom item templates.
+ *
+ * @deprecated Use `siTreeViewItem` instead.
+ *
+ * This directive is deprecated and will be removed in a future version.
+ *
+ * @example
+ * Migration Guide:
+ *
+ * Before (deprecated):
+ * ```html
+ * <si-tree-view>
+ *   <ng-template siTreeViewItemTemplate="root" let-item>
+ *     <div class="custom-item">Root {{ item.level }}</div>
+ *   </ng-template>
+ *   <ng-template siTreeViewItemTemplate="child" let-item>
+ *     <div class="custom-item">Child {{ item.level }}</div>
+ *   </ng-template>
+ * </si-tree-view>
+ * ```
+ *
+ * After (recommended):
+ * ```html
+ * <si-tree-view>
+ *   <ng-template siTreeViewItem let-item="treeItem">
+ *     <si-tree-view-item>
+ *      <div class="custom-item">
+ *       @if (item.level === 0) {
+ *         Root {{ item.level }}
+ *       } @else {
+ *         Child {{ item.level }}
+ *       }
+ *      </div>
+ *     </si-tree-view-item>
+ *   </ng-template>
+ * </si-tree-view>
+ * ```
+ */
 @Directive({
   selector: '[siTreeViewItemTemplate]'
 })
