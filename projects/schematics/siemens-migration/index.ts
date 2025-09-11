a0a23d5479e4fb04e0167b3824673ef00dfc1995
Karan Mistry <karan.mistry@siemens.com>
2025-09-11 19:45:35 +0530

    feat(schematics): add initial implementation of Siemens schematics
    - introduce vite to bundle schematics
    - added comprehensive import mappings for Simpl to Siemens component migration
    
    Example usage:
    
    ```bash
    ng generate @siemens/element-ng:siemens-migration
    ```

---
+++ projects/schematics/siemens-migration/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+
+import { MigrationOptions } from './model';
+import { importMigrationRule } from './ts-import-migration-rule';
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export const siemensMigration = (_options: MigrationOptions): Rule => {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.info('🚀 Starting Simpl to Siemens migration...');
+    const chainedRules = chain([importMigrationRule(_options)]);
+    return chainedRules(tree, context);
+  };
+};
---
+++ projects/schematics/siemens-migration/mappings/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+export * from './charts-ng-mappings';
+export * from './dashboards-ng-mappings';
+export * from './element-ng-mappings';
+export * from './element-translate-ng-mappings';
+export * from './maps-ng-mappings';
