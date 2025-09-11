4e29d7faf5f0f3208838d7b59842b511bdd9e568
Marco D'Auria <marco.dauria@siemens.com>
2025-09-11 23:55:51 +0200

    feat(buttons): introduce button groups

---
+++ src/app/examples/buttons/button-groups.ts
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { CdkMenuTrigger } from '@angular/cdk/menu';
+import { Component } from '@angular/core';
+import { SiMenuModule } from '@siemens/element-ng/menu';
+
+@Component({
+  selector: 'app-sample',
+  imports: [SiMenuModule, CdkMenuTrigger],
+  templateUrl: './button-groups.html'
+})
+export class SampleComponent {
+  splitOpen1 = false;
+  splitOpen2 = false;
+  splitOpen3 = false;
+  splitOpen4 = false;
+  splitOpen5 = false;
+  splitOpen6 = false;
+  splitOpen7 = false;
+
+  menuItems = [
+    { icon: 'element-download', text: 'Download as PDF' },
+    { icon: 'element-export', text: 'Export as CSV' },
+    { icon: 'element-share', text: 'Share' }
+  ];
+}
