bed32aa5f75414ab8518f5195c436e62f6edc486
Prasanna Rajan <prasanna.rajan@siemens.com>
2025-09-12 13:03:49 +0530

    chore(fomly): set OnPush as default for formly wrappers

--- projects/element-ng/formly/wrapper/si-formly-horizontal-wrapper.component.ts
+++ projects/element-ng/formly/wrapper/si-formly-horizontal-wrapper.component.ts
@@ -2,12 +2,13 @@
  * Copyright (c) Siemens 2016 - 2025
  * SPDX-License-Identifier: MIT
  */
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FieldWrapper, FormlyModule } from '@ngx-formly/core';
 
 @Component({
   selector: 'si-formly-horizontal-wrapper',
   imports: [FormlyModule],
-  templateUrl: './si-formly-horizontal-wrapper.component.html'
+  templateUrl: './si-formly-horizontal-wrapper.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SiFormlyHorizontalWrapperComponent extends FieldWrapper {}
--- projects/element-ng/formly/wrapper/si-formly-icon-wrapper.component.ts
+++ projects/element-ng/formly/wrapper/si-formly-icon-wrapper.component.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Siemens 2016 - 2025
  * SPDX-License-Identifier: MIT
  */
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FieldWrapper } from '@ngx-formly/core';
 import { SiTooltipDirective } from '@siemens/element-ng/tooltip';
 import { SiTranslatePipe } from '@siemens/element-translate-ng/translate';
@@ -11,6 +11,7 @@ import { SiTranslatePipe } from '@siemens/element-translate-ng/translate';
   selector: 'si-formly-icon-wrapper',
   imports: [SiTooltipDirective, SiTranslatePipe],
   templateUrl: './si-formly-icon-wrapper.component.html',
-  styleUrl: './si-formly-icon-wrapper.component.scss'
+  styleUrl: './si-formly-icon-wrapper.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SiFormlyIconWrapperComponent extends FieldWrapper {}
