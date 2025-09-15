3101869ebbfec54ad06db187e5f576d392a5095e
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-09-15 10:50:18 +0200

    feat(datepicker): support 12 hours clock for date-ranges

--- projects/element-ng/datepicker/components/test-helper.spec.ts
+++ projects/element-ng/datepicker/components/test-helper.spec.ts
@@ -97,6 +97,12 @@ export class CalendarTestHelper {
     return this.element.querySelector('si-timepicker input[name="seconds"]') as HTMLInputElement;
   }
 
+  getMeridian(): HTMLSelectElement | null {
+    return this.element.querySelector<HTMLSelectElement>(
+      'si-timepicker select[aria-label="Period"]'
+    );
+  }
+
   getTodayButton(): HTMLButtonElement {
     return this.element.querySelector('.today-button') as HTMLButtonElement;
   }
