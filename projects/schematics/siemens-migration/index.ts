370d22b167fdae9470ad254020f81e0d799620e7
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-09-18 07:24:42 +0200

    feat(schematics): migrate style imports to siemens
    - migrate scss files to the new siemens packages
    - migrate global styles to the brand package theming

--- projects/schematics/siemens-migration/index.ts
+++ projects/schematics/siemens-migration/index.ts
@@ -17,10 +17,22 @@ import {
   getImportNodes,
   getSymbols,
   createFullPathTree,
-  parseTsconfigFile
+  parseTsconfigFile,
+  getGlobalStyles
 } from '../utils';
 import { findComponentImportPath } from './mappings';
 import { MigrationOptions, Migrations } from './model';
+
+const styleReplacements = [
+  {
+    replace: `@import '@simpl/element-theme/`,
+    new: `@import '@siemens/element-theme/`
+  },
+  {
+    replace: `@use '@simpl/element-theme/`,
+    new: `@use '@siemens/element-theme/`
+  }
+];
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -41,6 +53,72 @@ export const siemensMigration = (_options: MigrationOptions): Rule => {
 
       const rule = rewriteImportsInFile(filePath, migrations);
       rules.push(rule);
+    }
+
+    const globalStyles = getGlobalStyles(tree);
+    for (const style of globalStyles) {
+      if (style.endsWith('.scss') || style.endsWith('.sass')) {
+        const content = tree.readText(style);
+        for (const pattern of [
+          /@use '@simpl\/element-theme\/src\/theme' with \(([\s\S]*?)\);/g,
+          /@use '@simpl\/element-ng\/simpl-element-ng' with \(([\s\S]*?)\);/g,
+          /@use '@simpl\/element-theme\/src\/theme';/g
+        ]) {
+          const match = pattern.exec(content);
+          if (match) {
+            rules.push(migrateScssImports(style, [{ replace: match[0], new: '' }]));
+          }
+        }
+
+        // Apply theme styles if not already present
+        const themeStyles = [
+          { insert: `@use '@simpl/brand/assets/fonts/styles/siemens-sans';` },
+          {
+            insert: `@use '@siemens/element-theme/src/theme' with (
+  $element-theme-default: 'siemens-brand',
+  $element-themes: (
+    'siemens-brand',
+    'element'
+  )
+);`,
+            pattern: /@use '@siemens\/element-theme\/src\/theme' with \(([\s\S]*?)\);/g
+          },
+          { insert: `@use '@siemens/element-ng/element-ng';` },
+          { insert: `@use '@siemens/element-theme/src/styles/themes';` },
+          { insert: `@use '@simpl/brand/dist/element-theme-siemens-brand-light' as brand-light;` },
+          { insert: `@use '@simpl/brand/dist/element-theme-siemens-brand-dark' as brand-dark;` },
+          {
+            insert: `@include themes.make-theme(brand-light.$tokens, 'siemens-brand');`,
+            pattern: /@include themes\.make-theme\(brand-light\.\$tokens, 'siemens-brand'\);/g
+          },
+          {
+            insert: `@include themes.make-theme(brand-dark.$tokens, 'siemens-brand', true);`,
+            pattern: /@include themes\.make-theme\(brand-dark\.\$tokens, 'siemens-brand', true\);/g
+          }
+        ];
+        let predecessor = '';
+        for (const themeEntry of themeStyles) {
+          const match = content.match(themeEntry.pattern ?? themeEntry.insert);
+          if (match) {
+            predecessor = match[0];
+            continue;
+          }
+
+          rules.push(applyGlobalStyles(style, predecessor, themeEntry.insert + '\n'));
+          predecessor = themeEntry.insert;
+        }
+      }
+    }
+
+    const scssFiles = discoverSourceFiles(tree, context, _options, '.scss');
+    for (const filePath of scssFiles) {
+      const content = tree.readText(filePath);
+      if (
+        content.includes(styleReplacements[0].replace) ||
+        content.includes(styleReplacements[1].replace)
+      ) {
+        rules.push(migrateScssImports(filePath));
+      }
     }
 
     const chainedRules = chain([...rules]);
@@ -121,7 +199,8 @@ const rewriteImportsInFile = (filePath: string, migrations: Migrations): Rule =>
 const discoverSourceFiles = (
   tree: Tree,
   context: SchematicContext,
-  options: MigrationOptions
+  options: MigrationOptions,
+  extension: string = '.ts'
 ): string[] => {
   const basePath = process.cwd().replace(/\\/g, '/');
 
@@ -138,7 +217,7 @@ const discoverSourceFiles = (
   for (const configPath of tsConfigs) {
     const tsConfigPath = resolve(basePath, configPath);
     const config = parseTsconfigFile(tsConfigPath, dirname(tsConfigPath), tsTree);
-    sourceFiles.push(...config.fileNames.filter(f => f.endsWith('.ts')));
+    sourceFiles.push(...config.fileNames.filter(f => f.endsWith(extension)));
   }
 
   // Filter all files which are in the path
@@ -149,4 +228,46 @@ const discoverSourceFiles = (
   }
 
   return Array.from(new Set(sourceFiles)).map(path => path.substring(basePath.length + 1));
+};
+
+const migrateScssImports = (
+  filePath: string,
+  replacements: { replace: string; new: string }[] = styleReplacements
+): Rule => {
+  return (tree: Tree): Tree => {
+    const recorder = tree.beginUpdate(filePath);
+    const content = tree.readText(filePath);
+    let offset = 0;
+    let hasMore = true;
+    while (hasMore) {
+      hasMore = false;
+      for (const replacement of replacements) {
+        const start = content.indexOf(replacement.replace, offset);
+        if (start >= 0) {
+          recorder.remove(start, replacement.replace.length);
+          recorder.insertLeft(start, replacement.new);
+          const size = replacement.new.length === 0 ? 1 : replacement.new.length;
+          offset = start + size;
+          hasMore = true;
+        }
+      }
+    }
+    tree.commitUpdate(recorder);
+    return tree;
+  };
+};
+
+const applyGlobalStyles = (filePath: string, anchor: string, insert: string): Rule => {
+  return (tree: Tree): Tree => {
+    const recorder = tree.beginUpdate(filePath);
+    const content = tree.readText(filePath);
+    let pos = content.indexOf(anchor) + anchor.length;
+    if (pos > 0) {
+      // If the insert position is not the file start we want to insert the next line after the new line
+      pos = content.indexOf('\n', pos) + 1;
+    }
+    recorder.insertRight(pos, insert);
+    tree.commitUpdate(recorder);
+    return tree;
+  };
 };
--- projects/schematics/utils/project-utils.ts
+++ projects/schematics/utils/project-utils.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 import { JsonValue, normalize, workspaces } from '@angular-devkit/core';
-import { ProjectDefinition } from '@angular-devkit/core/src/workspace';
+import { ProjectDefinition } from '@angular-devkit/core/src/workspace/definitions';
 import {
   Action,
   DirEntry,
@@ -40,6 +40,21 @@ export const getTsConfigPaths = (tree: Tree): string[] => {
   return [...buildPaths];
 };
 
+export const getGlobalStyles = (tree: Tree): string[] => {
+  const globalStyles = new Set<string>();
+
+  for (const target of getTargets(getWorkspace(tree))) {
+    if (target.options?.styles && Array.isArray(target.options.styles)) {
+      target.options.styles.forEach((style: JsonValue) => {
+        if (typeof style === 'string') {
+          globalStyles.add(normalize(style));
+        }
+      });
+    }
+  }
+
+  return [...globalStyles];
+};
 export const createFullPathTree = (basePath: string, tree: Tree): Tree => {
   const t: Tree = {
     branch: (): Tree => tree.branch(),
