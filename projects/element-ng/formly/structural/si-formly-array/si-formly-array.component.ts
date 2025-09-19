3c890212d165523e00e4c8a38cdcc6ee81f00141
Prasanna Rajan <prasanna.rajan@siemens.com>
2025-09-19 12:11:02 +0530

    chore(formly): set OnPush as default for structural components
    related to #668

--- projects/element-ng/formly/structural/si-formly-array/si-formly-array.component.ts
+++ projects/element-ng/formly/structural/si-formly-array/si-formly-array.component.ts
@@ -2,13 +2,14 @@
  * Copyright (c) Siemens 2016 - 2025
  * SPDX-License-Identifier: MIT
  */
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FieldArrayType, FormlyModule } from '@ngx-formly/core';
 import { SiTranslatePipe } from '@siemens/element-translate-ng/translate';
 
 @Component({
   selector: 'si-formly-array',
   imports: [FormlyModule, SiTranslatePipe],
-  templateUrl: './si-formly-array.component.html'
+  templateUrl: './si-formly-array.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SiFormlyArrayComponent extends FieldArrayType {}
