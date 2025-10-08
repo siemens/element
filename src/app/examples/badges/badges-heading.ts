50b5664b3174075f10ce7381a563bedb41c40d32
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-10-08 13:23:57 +0200

    docs(badge): update badge documentation

---
+++ src/app/examples/badges/badges-heading.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './badges-heading.html',
+  host: {
+    class: 'p-5 bg-base-1'
+  }
+})
+export class SampleComponent {}
