525267d8f14ead7701dba3a17c556e51a2cf0c6b
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-10-09 11:52:27 +0200

    docs(card): update card documentation

--- src/app/examples/si-card/card-grid.ts
+++ src/app/examples/si-card/card-grid.ts
@@ -9,7 +9,7 @@ import { LOG_EVENT } from '@siemens/live-preview';
 @Component({
   selector: 'app-sample',
   imports: [SiCardComponent],
-  templateUrl: './bootstrap-card-group.html',
+  templateUrl: './card-grid.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SampleComponent {
