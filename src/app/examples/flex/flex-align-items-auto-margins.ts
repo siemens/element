e84b1bda5109cf2c3012aaf459d99f0d22c17405
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-10-17 14:21:17 +0200

    docs(flex): document flex utils

---
+++ src/app/examples/flex/flex-align-items-auto-margins.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './flex-align-items-auto-margins.html',
+  styles: `
+    :host {
+      .d-flex > div {
+        padding: 0.5rem;
+      }
+      .example > div {
+        block-size: 80px;
+      }
+      .example div {
+        border: 1px solid rgb(from var(--element-action-primary) r g b / 0.3);
+      }
+    }
+  `,
+  host: {
+    class: 'p-5'
+  }
+})
+export class SampleComponent {}
---
+++ src/app/examples/flex/flex-justify-content.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './flex-justify-content.html',
+  styles: `
+    :host {
+      .d-flex > div {
+        padding: 0.5rem;
+      }
+      .example div {
+        border: 1px solid rgb(from var(--element-action-primary) r g b / 0.3);
+      }
+    }
+  `,
+  host: {
+    class: 'p-5'
+  }
+})
+export class SampleComponent {}
