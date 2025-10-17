1c9a63fddd7962079c19acaf26c4d44522dda015
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-10-17 14:21:17 +0200

    docs(flex): document flex utils

---
+++ src/app/examples/flex/flex-order.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './flex-order.html',
+  styles: `
+    :host {
+      .d-flex {
+        inline-size: 300px;
+      }
+      .d-flex > div {
+        padding: 0.5rem;
+      }
+      .example div {
+        border: 1px solid rgb(from var(--element-action-primary) r g b / 0.3);
+      }
+    }
+  `,
+  host: {
+    class: 'p-5'
+  }
+})
+export class SampleComponent {}
