65f8f21ed8a0cba6b57f2c961b98f37096e8319b
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-10-22 12:55:52 +0200

    docs(position): document position utils

---
+++ src/app/examples/position/position-center.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './position-center.html',
+  styles: `
+    :host {
+      > div {
+        block-size: 6rem;
+      }
+      div {
+        padding: 0.5rem;
+        background-color: rgb(from var(--element-ui-2) r g b / 0.3);
+      }
+    }
+  `,
+  host: {
+    class: 'p-5 d-flex flex-column gap-5'
+  }
+})
+export class SampleComponent {}
