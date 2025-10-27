57a7f353379f01fc2107a2c3d193b1c53e820414
Linus Schlumberger <linus.schlumberger@siemens.com>
2025-10-27 14:44:27 +0100

    docs(markdown-renderer): add markdown content example

---
+++ src/app/examples/si-markdown-renderer/si-markdown-renderer.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { SiMarkdownRendererComponent } from '@siemens/element-ng/markdown-renderer';
+
+@Component({
+  selector: 'app-sample',
+  imports: [SiMarkdownRendererComponent],
+  templateUrl: './si-markdown-renderer.html'
+})
+export class SampleComponent implements OnInit {
+  private readonly http = inject(HttpClient);
+  readonly markdownText = signal<string>('');
+
+  ngOnInit(): void {
+    this.http.get('assets/sample-markdown.md', { responseType: 'text' }).subscribe(text => {
+      this.markdownText.set(text);
+    });
+  }
+}
