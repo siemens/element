b47ee9a975e8fe23befed7a876e9cc2df648b06b
Karan Mistry <karan.mistry@siemens.com>
2025-10-28 15:15:11 +0530

    feat(schematics): remove legacy migration files and update tests for new component structure

--- projects/element-ng/schematics/migrations/to-legacy-migration/files/popover-template.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { SiPopoverDirective } from '@siemens/element-ng/popover';
-
-@Component({
-  selector: 'si-migration',
-  imports: [SiPopoverDirective],
-  templateUrl: './popover-template.html',
-})
-export class TemplateComponent {}
