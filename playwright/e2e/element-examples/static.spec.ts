85821429bed7aa6e8dee0c94b3f440eea2ad4452
Linus Schlumberger <linus.schlumberger@siemens.com>
2025-10-30 15:11:42 +0100

    test(chat-messages): add static VRT for chat-input

--- playwright/e2e/element-examples/static.spec.ts
+++ playwright/e2e/element-examples/static.spec.ts
@@ -114,3 +114,4 @@ test('si-chat-messages/si-ai-message', ({ si }) => si.static());
 test('si-chat-messages/si-user-message', ({ si }) => si.static());
 test('si-chat-messages/si-chat-message', ({ si }) => si.static());
 test('si-chat-messages/si-attachment-list', ({ si }) => si.static());
+test('si-chat-messages/si-chat-input', ({ si }) => si.static());
