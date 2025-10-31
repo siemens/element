7ba3a6aae1f77c16d445c9822afd79409fe931d8
Marco D'Auria <marco.dauria@siemens.com>
2025-10-31 17:00:12 +0100

    docs(buttons): remove leftovers from buttontypes

--- src/app/examples/buttons/buttontypes.ts
+++ src/app/examples/buttons/buttontypes.ts
@@ -2,13 +2,10 @@
  * Copyright (c) Siemens 2016 - 2025
  * SPDX-License-Identifier: MIT
  */
-import { CdkMenuTrigger } from '@angular/cdk/menu';
 import { Component } from '@angular/core';
-import { SiMenuModule } from '@siemens/element-ng/menu';
 
 @Component({
   selector: 'app-sample',
-  imports: [SiMenuModule, CdkMenuTrigger],
   templateUrl: './buttontypes.html',
   host: {
     class: 'bg-base-1'
@@ -16,16 +13,4 @@ import { SiMenuModule } from '@siemens/element-ng/menu';
 })
 export class SampleComponent {
   disabled = false;
-  splitOpen1 = false;
-  splitOpen2 = false;
-  splitOpen3 = false;
-  splitOpen4 = false;
-  splitOpen5 = false;
-  splitOpen6 = false;
-
-  menuItems = [
-    { icon: 'element-download', text: 'Download as PDF' },
-    { icon: 'element-download', text: 'Download as CSV' },
-    { icon: 'element-share', text: 'Share' }
-  ];
 }
