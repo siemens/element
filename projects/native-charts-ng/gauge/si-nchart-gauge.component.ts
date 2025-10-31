827356ef5bf177357b23d4ebb6f21ba612939879
akash.sonune <akash.sonune@siemens.com>
2025-10-31 18:51:56 +0530

    feat(native-charts/gauge): add support for label and value formatter

--- projects/native-charts-ng/gauge/si-nchart-gauge.component.ts
+++ projects/native-charts-ng/gauge/si-nchart-gauge.component.ts
@@ -145,6 +145,18 @@ export class SiNChartGaugeComponent implements OnInit, OnChanges {
    */
   readonly axisNumberOfDecimals = input(0);
   /**
+   * Custom formatter for axis labels.
+   * Takes precedence when specified, i.e. number of decimals needs
+   * to be set by the user using this formatter.
+   */
+  readonly axisLabelFormatter = input<(val: number) => string>();
+  /**
+   * Custom formatter for value.
+   * Takes precedence when specified, i.e. number of decimals needs
+   * to be set by the user using this formatter.
+   */
+  readonly valueFormatter = input<(val: number) => string>();
+  /**
    * Whether to show ticks
    *
    * @defaultValue true
@@ -219,7 +231,9 @@ export class SiNChartGaugeComponent implements OnInit, OnChanges {
       changes.max ||
       changes.showTicks ||
       changes.showRangeLabelsOutside ||
-      changes.segments
+      changes.segments ||
+      changes.axisLabelFormatter ||
+      changes.valueFormatter
     ) {
       calc = true;
     }
@@ -282,7 +296,10 @@ export class SiNChartGaugeComponent implements OnInit, OnChanges {
       }
     }
 
-    this.totalSumString = this.numberFormat.format(this.totalSum);
+    const formatter = this.valueFormatter();
+    this.totalSumString = formatter
+      ? formatter(this.totalSum)
+      : this.numberFormat.format(this.totalSum);
   }
 
   private calcSeriesSingle(): void {
@@ -328,7 +345,10 @@ export class SiNChartGaugeComponent implements OnInit, OnChanges {
     );
     this.addInternalSeries(this.startAngle(), series, false, activeSegment?.colorToken);
 
-    this.totalSumString = this.numberFormat.format(this.totalSum);
+    const formatter = this.valueFormatter();
+    this.totalSumString = formatter
+      ? formatter(this.totalSum)
+      : this.numberFormat.format(this.totalSum);
   }
 
   private addTick(angle: number, title: number, boundary: boolean): void {
@@ -345,9 +365,10 @@ export class SiNChartGaugeComponent implements OnInit, OnChanges {
       textPos.y += 6;
     }
 
+    const axisLabelFormatter = this.axisLabelFormatter();
     this.ticks.push({
       path: this.showTicks() ? this.makeTickPath(angle) : '',
-      label: this.axisNumberFormat.format(title),
+      label: axisLabelFormatter ? axisLabelFormatter(title) : this.axisNumberFormat.format(title),
       textPos
     });
   }
