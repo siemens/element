0aa356012162bb96ce1305f1ef3bf05610076a5b
Karan Mistry <karan.mistry@siemens.com>
2025-11-05 17:20:35 +0530

    feat(schematics): implement migration rule for wizard API and add related test files

--- projects/element-ng/schematics/migrations/index.ts
+++ projects/element-ng/schematics/migrations/index.ts
@@ -7,10 +7,15 @@ import { Tree, SchematicContext, Rule, chain } from '@angular-devkit/schematics'
 
 import { actionModalMigrationRule } from './action-modal-migration/index.js';
 import { elementMigrationRule } from './element-migration/index.js';
+import { wizardMigrationRule } from './wizard-migration/index.js';
 
 export const v47to48Migration = (options: { path: string }): Rule => {
   return (tree: Tree, context: SchematicContext) => {
     context.logger.info('🚀 Starting migration from v47 to v48...');
-    return chain([actionModalMigrationRule(options), elementMigrationRule(options)])(tree, context);
+    return chain([
+      actionModalMigrationRule(options),
+      elementMigrationRule(options),
+      wizardMigrationRule(options)
+    ])(tree, context);
   };
 };
---
+++ projects/element-ng/schematics/migrations/wizard-migration/files/wizard-inline-template.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { SiWizardModule } from '@siemens/element-ng/wizard';
+
+@Component({
+  selector: 'app-sample',
+  template:`<si-wizard [inlineNavigation]="false">
+  <si-wizard-step heading="Step 1">
+    <h1 class="text-center">Step 1</h1>
+  </si-wizard-step>
+  <si-wizard-step heading="Step 2">
+    <h1 class="text-center">Step 2</h1>
+  </si-wizard-step>
+</si-wizard>
+`,
+  imports: [SiWizardModule],
+  standalone: true
+})
+export class SampleComponent {}
