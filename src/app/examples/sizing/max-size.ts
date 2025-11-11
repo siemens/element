94710d101e8ef714d09654dd83bc8ecfdd43be93
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-11-11 10:18:09 +0100

    docs(sizing): document sizing utils

---
+++ src/app/examples/sizing/max-size.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './max-size.html',
+  styles: `
+    :host {
+      div {
+        background-color: rgb(from var(--element-ui-2) r g b / 0.3);
+      }
+    }
+  `,
+  host: {
+    class: 'p-5'
+  }
+})
+export class SampleComponent {}
