d3d5d29182ad421f81b405657f636bd24ea4d2b1
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-11-11 11:35:58 +0100

    docs(image-cropper): document the usage of ngx-image-cropper
    closes #974

---
+++ src/app/examples/si-photo-upload/image-cropping.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { SiImageCropperStyleComponent } from '@siemens/element-ng/photo-upload';
+import { LOG_EVENT } from '@siemens/live-preview';
+import { ImageCropperComponent, ImageCroppedEvent } from 'ngx-image-cropper';
+
+@Component({
+  selector: 'app-sample',
+  imports: [ImageCropperComponent, SiImageCropperStyleComponent],
+  templateUrl: './image-cropping.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { 'class': 'p-5' }
+})
+export class SampleComponent {
+  protected readonly logEvent = inject(LOG_EVENT);
+  protected imageSource = 'assets/images/beach.jpg';
+
+  protected onImageCropped(event: ImageCroppedEvent): void {
+    this.logEvent('Image cropped', event);
+  }
+}
