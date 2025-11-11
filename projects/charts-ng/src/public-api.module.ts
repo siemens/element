67a0ad4481ab659b72d9b9f413545106548a285b
akash.sonune <akash.sonune@siemens.com>
2025-11-11 18:03:40 +0530

    refactor(charts): remove unnecessary imports

--- projects/charts-ng/src/public-api.module.ts
+++ projects/charts-ng/src/public-api.module.ts
@@ -2,13 +2,11 @@
  * Copyright (c) Siemens 2016 - 2025
  * SPDX-License-Identifier: MIT
  */
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { SiChartCartesianComponent } from './components/si-chart-cartesian/si-chart-cartesian.component';
 import { SiChartCircleComponent } from './components/si-chart-circle/si-chart-circle.component';
 import { SiChartGaugeComponent } from './components/si-chart-gauge/si-chart-gauge.component';
-import { SiChartLoadingSpinnerComponent } from './components/si-chart-loading-spinner/si-chart-loading-spinner.component';
 import { SiChartProgressBarComponent } from './components/si-chart-progress-bar/si-chart-progress-bar.component';
 import { SiChartProgressComponent } from './components/si-chart-progress/si-chart-progress.component';
 import { SiChartSankeyComponent } from './components/si-chart-sankey/si-chart-sankey.component';
@@ -18,12 +16,10 @@ import { SiCustomLegendComponent } from './components/si-custom-legend/si-custom
 
 @NgModule({
   imports: [
-    CommonModule,
     SiChartCartesianComponent,
     SiChartCircleComponent,
     SiChartComponent,
     SiChartGaugeComponent,
-    SiChartLoadingSpinnerComponent,
     SiChartProgressBarComponent,
     SiChartProgressComponent,
     SiChartSankeyComponent,
