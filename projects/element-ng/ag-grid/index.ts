b0cf0329c22a6989db0728b5d10dc7f81fea5054
Karan Mistry <karan.mistry@siemens.com>
2025-11-14 18:32:37 +0530

    feat: add ag-grid-community as an optional dependency and simplify theme creation

--- projects/element-ng/ag-grid/index.ts
+++ projects/element-ng/ag-grid/index.ts
@@ -3,23 +3,11 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { createPart, createTheme } from 'ag-grid-community';
+import { createTheme } from 'ag-grid-community';
 
 import { elementIconOverrides } from './parts/icon-set';
 
-export const elementTheme = createTheme()
-  .withPart(elementIconOverrides)
-  .withPart(
-    createPart({
-      feature: 'checkboxStyle',
-      css: `
-      .ag-checkbox-input{
-@extend %form-check-base;
-      }
-      `
-    })
-  )
-  .withParams({
-    fontFamily: '"SiemensSans Pro", sans-serif',
-    iconSize: 18
-  });
+export const elementTheme = createTheme().withPart(elementIconOverrides).withParams({
+  fontFamily: '"SiemensSans Pro", sans-serif',
+  iconSize: 18
+});
