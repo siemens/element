365fc714be2dddf1c34def3c92af158012a82e89
Karan Mistry <karan.mistry@siemens.com>
2025-11-19 17:58:59 +0530

    feat(ag-grid): refactor icon mapping to use createIconMap for cleaner code

--- projects/element-ng/ag-grid/parts/icon-set.ts
+++ projects/element-ng/ag-grid/parts/icon-set.ts
@@ -23,66 +23,42 @@ import {
 } from '@siemens/element-ng/icon';
 import { iconOverrides } from 'ag-grid-community';
 
+const createIconMap = (icons: Record<string, string>): Record<string, { svg: string }> =>
+  Object.fromEntries(
+    Object.entries(icons).map(([key, dataUri]) => [key, { svg: dataUri.split(',')[1] }])
+  );
+
 export const elementIconOverrides = iconOverrides({
   type: 'image',
   mask: true,
-  icons: {
-    'menu': {
-      svg: elementMenu.split(',')[1]
-    },
-    'filter': {
-      svg: elementFilter.split(',')[1]
-    },
-    'asc': {
-      svg: elementSortUp.split(',')[1]
-    },
-    'desc': {
-      svg: elementSortDown.split(',')[1]
-    },
-    'grip': {
-      svg: elementMenu.split(',')[1]
-    },
-    'pin': {
-      svg: elementPin.split(',')[1]
-    },
-    'columns': {
-      svg: elementColumnMove.split(',')[1]
-    },
-    'cut': {
-      svg: elementCut.split(',')[1]
-    },
-    'copy': {
-      svg: elementCopy.split(',')[1]
-    },
-    'paste': {
-      svg: elementPaste.split(',')[1]
-    },
-    'export': {
-      svg: elementExport.split(',')[1]
-    },
-    'csv': {
-      svg: elementDocument.split(',')[1]
-    },
-    'excel': {
-      svg: elementDocument.split(',')[1]
-    },
-    'menu-alt': {
-      svg: elementOptionsVertical.split(',')[1]
-    },
-    'small-down': {
-      svg: elementDown1Filled.split(',')[1]
-    },
-    'first': {
-      svg: elementBeginFilled.split(',')[1]
-    },
-    'last': {
-      svg: elementEndFilled.split(',')[1]
-    },
-    'previous': {
-      svg: elementLeft2.split(',')[1]
-    },
-    'next': {
-      svg: elementRight2.split(',')[1]
-    }
-  }
+  icons: createIconMap({
+    // Grid actions
+    menu: elementMenu,
+    'menu-alt': elementOptionsVertical,
+    grip: elementMenu,
+
+    // Column operations
+    filter: elementFilter,
+    asc: elementSortUp,
+    desc: elementSortDown,
+    pin: elementPin,
+    columns: elementColumnMove,
+
+    // Clipboard operations
+    cut: elementCut,
+    copy: elementCopy,
+    paste: elementPaste,
+
+    // Export operations
+    export: elementExport,
+    csv: elementDocument,
+    excel: elementDocument,
+
+    // Navigation
+    'small-down': elementDown1Filled,
+    first: elementBeginFilled,
+    last: elementEndFilled,
+    previous: elementLeft2,
+    next: elementRight2
+  })
 });
