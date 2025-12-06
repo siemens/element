2bc39c91180c960d08771c7127e33afcbc684544
Linus Schlumberger <linus.schlumberger@siemens.com>
2025-12-06 14:46:26 +0100

    feat(chat-messages): add support for code syntax highlighting in markdown renderer

--- projects/element-ng/markdown-renderer/si-markdown-renderer.component.ts
+++ projects/element-ng/markdown-renderer/si-markdown-renderer.component.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 import { CommonModule } from '@angular/common';
-import { Component, effect, inject, input, ElementRef } from '@angular/core';
+import { Component, computed, effect, inject, input, ElementRef } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { injectSiTranslateService, t } from '@siemens/element-translate-ng/translate';
 
@@ -42,6 +42,17 @@ export class SiMarkdownRendererComponent {
   readonly disableDownloadButton = input<boolean>(false);
 
   /**
+   * Optional syntax highlighter function for code blocks.
+   * Receives code content and optional language, returns an HTML content string to display inside of the code block or undefined to use default rendering.
+   * The returned code is sanitized before insertion.
+   * Make sure that the required styles/scripts for the syntax highlighter are included in your application.
+   * @defaultValue undefined
+   */
+  readonly syntaxHighlighter = input<
+    ((code: string, language?: string) => string | undefined) | undefined
+  >(undefined);
+
+  /**
    * Label for the copy button.
    * @defaultValue
    * ```
@@ -61,21 +72,27 @@ export class SiMarkdownRendererComponent {
     t(() => $localize`:@@SI_MARKDOWN_RENDERER.DOWNLOAD:Download CSV`)
   );
 
+  private readonly markdownRenderer = computed(() => {
+    const highlighterFn = this.syntaxHighlighter();
+
+    const options: MarkdownRendererOptions | undefined = {
+      copyCodeButton: !this.disableCopyButton() ? this.copyButtonLabel() : undefined,
+      downloadTableButton: !this.disableDownloadButton() ? this.downloadButtonLabel() : undefined,
+      syntaxHighlighter: highlighterFn,
+      translateSync: this.translateService.translateSync.bind(this.translateService)
+    };
+
+    return getMarkdownRenderer(this.sanitizer, options);
+  });
+
   constructor() {
     effect(() => {
       const contentValue = this.text();
       const containerEl = this.hostElement.nativeElement;
-
-      const options: MarkdownRendererOptions | undefined = {
-        copyCodeButton: !this.disableCopyButton() ? this.copyButtonLabel() : undefined,
-        downloadTableButton: !this.disableDownloadButton() ? this.downloadButtonLabel() : undefined,
-        translateSync: this.translateService.translateSync.bind(this.translateService)
-      };
-
-      const markdownRenderer = getMarkdownRenderer(this.sanitizer, options);
+      const renderer = this.markdownRenderer();
 
       if (containerEl) {
-        const formattedNode = markdownRenderer(contentValue);
+        const formattedNode = renderer(contentValue);
         containerEl.innerHTML = '';
         containerEl.appendChild(formattedNode);
       }
