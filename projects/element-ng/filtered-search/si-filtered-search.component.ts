968f94fb3f8d34ba6f06f127569cea7c4f98aeb6
akash.sonune <akash.sonune@siemens.com>
2025-12-09 21:31:35 +0530

    refactor(filtered-search): remove deprecated input readonly
    BREAKING CHANGE: Removed `SiFilteredSearchComponent.readonly` input. Use `SiFilteredSearchComponent.disabled` instead.

--- projects/element-ng/filtered-search/si-filtered-search.component.ts
+++ projects/element-ng/filtered-search/si-filtered-search.component.ts
@@ -110,14 +110,6 @@ export class SiFilteredSearchComponent implements OnInit, OnChanges {
    */
   readonly disabled = input(false, { transform: booleanAttribute });
   /**
-   * Do not allow changes. Search can still be triggered.
-   *
-   * @deprecated Use {@link disabled} instead.
-   *
-   * @defaultValue false
-   */
-  readonly readonly = input(false, { transform: booleanAttribute });
-  /**
    * Limit criteria to the predefined ones.
    *
    * @defaultValue false
@@ -352,7 +344,7 @@ export class SiFilteredSearchComponent implements OnInit, OnChanges {
     }
   });
 
-  private readonly isReadOnly = computed(() => this.readonly() || this.disabled());
+  private readonly isReadOnly = computed(() => this.disabled());
 
   constructor() {
     this.dataSource = this.typeaheadInputChange.pipe(
