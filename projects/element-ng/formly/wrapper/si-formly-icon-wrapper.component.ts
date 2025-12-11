eb8e2152c1c885232c7908060a859e9b35936a62
Maximilian Koeller <maximilian.koeller@siemens.com>
2025-12-11 14:38:53 +0100

    refactor(icons): always use si-icon for app defined icons

--- projects/element-ng/formly/wrapper/si-formly-icon-wrapper.component.ts
+++ projects/element-ng/formly/wrapper/si-formly-icon-wrapper.component.ts
@@ -4,12 +4,13 @@
  */
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FieldWrapper } from '@ngx-formly/core';
+import { SiIconComponent } from '@siemens/element-ng/icon';
 import { SiTooltipDirective } from '@siemens/element-ng/tooltip';
 import { SiTranslatePipe } from '@siemens/element-translate-ng/translate';
 
 @Component({
   selector: 'si-formly-icon-wrapper',
-  imports: [SiTooltipDirective, SiTranslatePipe],
+  imports: [SiIconComponent, SiTooltipDirective, SiTranslatePipe],
   templateUrl: './si-formly-icon-wrapper.component.html',
   styleUrl: './si-formly-icon-wrapper.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
