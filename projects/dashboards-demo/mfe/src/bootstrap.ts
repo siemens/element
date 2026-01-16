ae99172178cf4be35914043e65512d25a50e5e98
chintankavathia <chintankumar.kavathia@siemens.com>
2026-01-16 10:05:06 +0530

    chore(dashboards-demo): drop zone.js

--- projects/dashboards-demo/mfe/src/bootstrap.ts
+++ projects/dashboards-demo/mfe/src/bootstrap.ts
@@ -2,9 +2,8 @@
  * Copyright (c) Siemens 2016 - 2025
  * SPDX-License-Identifier: MIT
  */
-import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, { providers: [provideZoneChangeDetection()] });
+bootstrapApplication(AppComponent);
