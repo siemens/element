1798f7875175fc6b266cb72134b84d0d01ccad19
Maximilian Koeller <maximilian.koeller@siemens.com>
2026-01-27 14:31:50 +0100

    docs(date-range): wrap `si-date-range` in `si-form-item`

--- src/app/examples/si-datepicker/si-date-range.ts
+++ src/app/examples/si-datepicker/si-date-range.ts
@@ -6,10 +6,11 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { DateRange, SiDateRangeComponent } from '@siemens/element-ng/datepicker';
+import { SiFormItemComponent } from '@siemens/element-ng/form';
 
 @Component({
   selector: 'app-sample',
-  imports: [CommonModule, SiDateRangeComponent, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, SiDateRangeComponent, SiFormItemComponent],
   templateUrl: './si-date-range.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
