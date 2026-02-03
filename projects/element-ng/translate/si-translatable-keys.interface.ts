eaa243ec0ee6dae43e01358e367f651f5a9246c7
michael-smt <139211597+michael-smt@users.noreply.github.com>
2026-02-03 15:28:07 +0100

    feat(landing-page): remove registration introduction text due to ux guidelines

--- projects/element-ng/translate/si-translatable-keys.interface.ts
+++ projects/element-ng/translate/si-translatable-keys.interface.ts
@@ -168,7 +168,6 @@ export interface SiTranslatableKeys {
   'SI_LOGIN_BASIC.LOGIN'?: string;
   'SI_LOGIN_BASIC.NEXT'?: string;
   'SI_LOGIN_BASIC.PASSWORD'?: string;
-  'SI_LOGIN_BASIC.REGISTER_NOW_INTRO'?: string;
   'SI_LOGIN_BASIC.USERNAME'?: string;
   'SI_LOGIN_SINGLE-SIGN-ON.LOGIN_SIGN_UP'?: string;
   'SI_MAIN_DETAIL_CONTAINER.BACK'?: string;
