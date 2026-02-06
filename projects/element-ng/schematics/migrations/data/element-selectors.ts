0cf8007b86a2264f6f6b18dd3971f97667d9a741
Karan Mistry <karan.mistry@siemens.com>
2026-02-06 13:22:02 +0530

    feat(schematics): add new schematics data for version 49

--- projects/element-ng/schematics/migrations/data/element-selectors.ts
+++ projects/element-ng/schematics/migrations/data/element-selectors.ts
@@ -7,6 +7,18 @@ export interface ElementSelectorInstruction {
   replace: string;
   /** The new name for the element. */
   replaceWith: string;
+  /** Optional default attributes to add to the new element */
+  defaultAttributes?: { name: string; value: string }[];
 }
 
-export const ELEMENT_SELECTORS_MIGRATION: ElementSelectorInstruction[] = [];
+export const ELEMENT_SELECTORS_MIGRATION: ElementSelectorInstruction[] = [
+  // v48 to v49
+  {
+    replace: 'si-unauthorized-page',
+    replaceWith: 'si-info-page',
+    defaultAttributes: [
+      { name: 'icon', value: 'element-warning-filled' },
+      { name: 'iconColor', value: 'status-warning' }
+    ]
+  }
+];
