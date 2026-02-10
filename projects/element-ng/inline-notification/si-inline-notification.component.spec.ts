800ee0171cd8e6ebbda9bd8a2b3584697b0b876f
Marco D'Auria <marco.dauria@siemens.com>
2026-02-10 17:54:54 +0100

    fix(inline-notification): remove colon from heading

--- projects/element-ng/inline-notification/si-inline-notification.component.spec.ts
+++ projects/element-ng/inline-notification/si-inline-notification.component.spec.ts
@@ -70,7 +70,7 @@ describe('SiInlineNotificationComponent', () => {
     fixture.detectChanges();
 
     expect(element.querySelector<HTMLElement>('.alert strong')!.innerText).toBe(
-      'translated=>MSG.HEADING-{"param":"something"}:'
+      'translated=>MSG.HEADING-{"param":"something"}'
     );
     expect(element.querySelector<HTMLElement>('div > span:not(.icon)')!.innerText).toBe(
       'translated=>MSG.MESSAGE-{"param":"something"}'
