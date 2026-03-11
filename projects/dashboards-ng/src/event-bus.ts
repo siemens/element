f86927b1f9b3cef3781f7caeaf7aef5e2529f309
chintankavathia <chintankumar.kavathia@siemens.com>
2026-03-11 14:08:20 +0530

    feat(dashboards-ng): event bus for widget communication

--- projects/dashboards-ng/src/event-bus.ts
+++ projects/dashboards-ng/src/event-bus.ts
@@ -3,20 +3,13 @@
  * SPDX-License-Identifier: MIT
  */
 import { Injectable } from '@angular/core';
-import type { Theme } from '@siemens/element-ng/theme';
 
-import { EventBusBase } from './event-bus.base';
+import { EventBusBase, EventType } from './event-bus.base';
 
-export interface EventTypes<Filter = unknown, Export = unknown> {
-  filter: Filter;
-  export: Export;
-  languageChange: string;
-  themeChange: Theme;
-  dateRangeChange: { start: Date; end: Date };
-  timeZoneChange: string;
-}
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventBus<ET extends EventTypes = EventTypes> extends EventBusBase<ET> {}
+export class EventBus<ET extends {name: string; data: unknown} = EventType> extends EventBusBase<ET> {}
+
+export type { EventType };
