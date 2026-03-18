badf73d14a5500edc89eaf98beac824f3d196e3e
Bajohr, Rayk <rayk.bajohr@siemens.com>
2026-03-18 12:22:56 +0100

    test(tree): remove usage of waitForAsync

--- projects/element-ng/tree-view/si-tree-view-item-height.service.spec.ts
+++ projects/element-ng/tree-view/si-tree-view-item-height.service.spec.ts
@@ -2,18 +2,18 @@
  * Copyright (c) Siemens 2016 - 2026
  * SPDX-License-Identifier: MIT
  */
-import { inject, TestBed, waitForAsync } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { SiTreeViewItemHeightService } from './si-tree-view-item-height.service';
 import { SiTreeViewService } from './si-tree-view.service';
 
 export const main = (): void => {
   describe('SiTreeViewItemHeightService', () => {
-    beforeEach(waitForAsync(() => {
-      TestBed.configureTestingModule({
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
         providers: [SiTreeViewService, SiTreeViewItemHeightService]
       }).compileComponents();
-    }));
+    });
 
     it('should create SiTreeViewItemHeightService', inject(
       [SiTreeViewItemHeightService],
