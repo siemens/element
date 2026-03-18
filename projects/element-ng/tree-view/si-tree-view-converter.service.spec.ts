2cb045f7e99d6dd2fcd153980b4c87c4f1157093
Bajohr, Rayk <rayk.bajohr@siemens.com>
2026-03-18 12:22:56 +0100

    test(tree): remove usage of waitForAsync

--- projects/element-ng/tree-view/si-tree-view-converter.service.spec.ts
+++ projects/element-ng/tree-view/si-tree-view-converter.service.spec.ts
@@ -2,18 +2,18 @@
  * Copyright (c) Siemens 2016 - 2026
  * SPDX-License-Identifier: MIT
  */
-import { inject, TestBed, waitForAsync } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { SiTreeViewConverterService } from './si-tree-view-converter.service';
 import { SiTreeViewService } from './si-tree-view.service';
 
 export const main = (): void => {
   describe('SiTreeViewConverterService', () => {
-    beforeEach(waitForAsync(() => {
-      TestBed.configureTestingModule({
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
         providers: [SiTreeViewService, SiTreeViewConverterService]
       }).compileComponents();
-    }));
+    });
 
     it('should create SiTreeViewConverterService', inject(
       [SiTreeViewConverterService],
