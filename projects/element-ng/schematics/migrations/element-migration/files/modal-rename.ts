2dcc76e16db31a46ba664580b6e38eaedd09a567
akash.sonune <akash.sonune@siemens.com>
2026-03-30 15:49:51 +0530

    feat(schematics): add migration schematic for modal options from `initialState` to `inputValues`

---
+++ projects/element-ng/schematics/migrations/element-migration/files/modal-rename.ts
@@ -0,0 +1,25 @@
+import { Component, inject, Input } from '@angular/core';
+import { ModalRef, SiModalService  } from '@siemens/element-ng/modal';
+
+@Component({
+  selector: 'modal-content',
+  template: 'modal',
+  standalone: true,
+})
+export class ModalContent {
+  @Input()
+  protected fileFormat = ''
+}
+
+export class MyClass {
+  private readonly modalService = inject(SiModalService);
+  private modalRef: ModalRef<ModalContent>;
+
+  myMethod() {
+    this.modalRef = this.modalService.show(ModalContent, {
+      class: 'modal-dialog-centered',
+      initialState: { fileFormat: 'pdf' },
+      ignoreBackdropClick: false,
+    });
+  }
+}
