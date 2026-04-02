2420a049a0d09d1eaa3f3899ca7aba262693aea6
Karan Mistry <karan.mistry@siemens.com>
2026-04-02 10:24:24 +0530

    refactor(dashboards-ng): correct typos in dashboards-related components and tests

--- projects/dashboards-ng/src/model/si-widget-storage.spec.ts
+++ projects/dashboards-ng/src/model/si-widget-storage.spec.ts
@@ -17,24 +17,24 @@ describe('SiDefaultWidgetStorage', () => {
 
   describe('#load()', () => {
     it('with dashboardId should return widget config observable', () => {
-      const widgitConfigs$ = widgetStorage.load('id');
-      expect(widgitConfigs$).toBeDefined();
+      const widgetConfigs$ = widgetStorage.load('id');
+      expect(widgetConfigs$).toBeDefined();
     });
     it('without dashboardId should return widget config observable', () => {
-      const widgitConfigs$ = widgetStorage.load();
-      expect(widgitConfigs$).toBeDefined();
+      const widgetConfigs$ = widgetStorage.load();
+      expect(widgetConfigs$).toBeDefined();
     });
     it('with different ids should return different objects', () => {
-      const widgitConfigs$ = widgetStorage.load();
-      const widgitConfigs1$ = widgetStorage.load('1');
-      const widgitConfigs2$ = widgetStorage.load('2');
-      expect(widgitConfigs$).not.toBe(widgitConfigs1$);
-      expect(widgitConfigs$).not.toBe(widgitConfigs2$);
+      const widgetConfigs$ = widgetStorage.load();
+      const widgetConfigs1$ = widgetStorage.load('1');
+      const widgetConfigs2$ = widgetStorage.load('2');
+      expect(widgetConfigs$).not.toBe(widgetConfigs1$);
+      expect(widgetConfigs$).not.toBe(widgetConfigs2$);
     });
     it('with same ids should return same objects', () => {
-      const widgitConfigs1$ = widgetStorage.load('1');
-      const widgitConfigs2$ = widgetStorage.load('1');
-      expect(widgitConfigs1$).toBe(widgitConfigs2$);
+      const widgetConfigs1$ = widgetStorage.load('1');
+      const widgetConfigs2$ = widgetStorage.load('1');
+      expect(widgetConfigs1$).toBe(widgetConfigs2$);
     });
   });
 });
