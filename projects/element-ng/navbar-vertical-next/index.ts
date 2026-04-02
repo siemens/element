88a88e877348d51e6a4bda23973a540a5e7fde4f
Karan Mistry <karan.mistry@siemens.com>
2026-04-02 16:01:02 +0530

    feat(navbar-vertical-next): add navbar-vertical-next component
    - Duplicate navbar-vertical to navbar-vertical-next to implement updated version for the same
    - No code changes are done, only copied the folder and renamed the selector's where ever necessary

---
+++ projects/element-ng/navbar-vertical-next/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Copyright (c) Siemens 2016 - 2026
+ * SPDX-License-Identifier: MIT
+ */
+export * from './si-navbar-vertical-next.component';
+export * from './si-navbar-vertical-next.module';
+export * from './si-navbar-vertical-next.model';
