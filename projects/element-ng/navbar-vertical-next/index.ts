610beaef05e027c1f51d3453fd27aec51d97a380
Karan Mistry <karan.mistry@siemens.com>
2026-04-21 20:23:10 +0530

    refactor(navbar-vertical-next): extract search functionality into a new component
    Extract search functionality from SiNavbarVerticalNextComponent into
    a new SiNavbarVerticalNextSearchComponent. Search is now used via
    content projection (<si-navbar-vertical-next-search>) instead of
    input/output bindings (searchable, searchPlaceholder, searchDebounceTime,
    searchEvent).
    
    See #1792

--- projects/element-ng/navbar-vertical-next/index.ts
+++ projects/element-ng/navbar-vertical-next/index.ts
@@ -3,10 +3,11 @@
  * SPDX-License-Identifier: MIT
  */
 export * from './si-navbar-vertical-items-next.component';
-export * from './si-navbar-vertical-next.component';
 export * from './si-navbar-vertical-next-divider.component';
-export * from './si-navbar-vertical-next-group.component';
 export * from './si-navbar-vertical-next-group-trigger.directive';
+export * from './si-navbar-vertical-next-group.component';
 export * from './si-navbar-vertical-next-header.component';
 export * from './si-navbar-vertical-next-item.component';
+export * from './si-navbar-vertical-next-search.component';
+export * from './si-navbar-vertical-next.component';
 export * from './si-navbar-vertical-next.module';
