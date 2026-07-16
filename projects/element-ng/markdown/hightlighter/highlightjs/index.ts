7b5f59f761e53c926060539927a81e86996e5c85
Daniel Ritz <daniel.ritz@shellybits.ch>
2026-07-16 18:54:48 +0200

    feat(markdown): markdown rendering component

---
+++ projects/element-ng/markdown/hightlighter/highlightjs/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Copyright (c) Siemens 2016 - 2026
+ * SPDX-License-Identifier: MIT
+ */
+export * from './si-markdown-highlightjs.types';
+export * from './si-markdown-highlightjs.highlighter';
