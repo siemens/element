459574501b5c11d47d93ca20ebe2a73ae8d7330f
Bajohr, Rayk <rayk.bajohr@siemens.com>
2026-07-28 14:00:00 +0200

    docs: improve dropdown documentation

--- src/app/examples/dropdown/dropdown-with-overlay.ts
+++ src/app/examples/dropdown/dropdown-with-overlay.ts
@@ -4,17 +4,19 @@
  */
 import { A11yModule } from '@angular/cdk/a11y';
 import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { elementDown2 } from '@siemens/element-icons';
+import { addIcons, SiIconComponent } from '@siemens/element-ng/icon';
 import { LOG_EVENT } from '@siemens/live-preview';
 
 @Component({
   selector: 'app-sample',
-  imports: [CdkOverlayOrigin, CdkConnectedOverlay, A11yModule],
+  imports: [A11yModule, CdkOverlayOrigin, CdkConnectedOverlay, SiIconComponent],
   templateUrl: './dropdown-with-overlay.html',
   host: { class: 'p-5' }
 })
 export class SampleComponent {
-  logEvent = inject(LOG_EVENT);
-
-  open = false;
+  protected readonly logEvent = inject(LOG_EVENT);
+  protected readonly icons = addIcons({ elementDown2 });
+  protected readonly open = signal(false);
 }
