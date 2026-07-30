7834d2f4957bb7eaef1a7f5f58de6176052343d4
Karan Mistry <karan.mistry@siemens.com>
2026-07-30 19:03:07 +0530

    feat(info-page): adjust illustration size and spacing defaults
    - Adjust gap between the image and title
    - Update playwright snapshots

--- src/app/examples/si-info-page/si-info-page-illustration.ts
+++ src/app/examples/si-info-page/si-info-page-illustration.ts
@@ -2,13 +2,14 @@
  * Copyright (c) Siemens 2016 - 2026
  * SPDX-License-Identifier: MIT
  */
+import { NgOptimizedImage } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { SiInfoPageComponent } from '@siemens/element-ng/info-page';
 import { LOG_EVENT } from '@siemens/live-preview';
 
 @Component({
   selector: 'app-sample',
-  imports: [SiInfoPageComponent],
+  imports: [SiInfoPageComponent, NgOptimizedImage],
   templateUrl: './si-info-page-illustration.html'
 })
 export class SampleComponent {
