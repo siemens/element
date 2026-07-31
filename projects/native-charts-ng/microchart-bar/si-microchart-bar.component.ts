2d113f749852efe498bc103f72783bf3c011fb4f
Bajohr, Rayk <rayk.bajohr@siemens.com>
2026-07-31 14:11:22 +0200

    docs: provide missing input descriptions part 12

--- projects/native-charts-ng/microchart-bar/si-microchart-bar.component.ts
+++ projects/native-charts-ng/microchart-bar/si-microchart-bar.component.ts
@@ -30,9 +30,17 @@ interface SeriesInternal {
   templateUrl: './si-microchart-bar.component.html'
 })
 export class SiMicrochartBarComponent {
-  /** @defaultValue 64 */
+  /**
+   * Chart width in pixels.
+   *
+   * @defaultValue 64
+   */
   readonly width = input<number>(64);
-  /** @defaultValue 24 */
+  /**
+   * Chart height in pixels.
+   *
+   * @defaultValue 24
+   */
   readonly height = input<number>(24);
   /**
    * Microchart bar series.
