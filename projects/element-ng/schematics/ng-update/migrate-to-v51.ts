097f9ab6a00240924bf07fc78ab52e98f78db72b
chintankavathia <chintankumar.kavathia@siemens.com>
2026-08-11 13:57:29 +0530

    refactor(schematic): remove icon migration rules
    not needed for v51

--- projects/element-ng/schematics/ng-update/migrate-to-v51.ts
+++ projects/element-ng/schematics/ng-update/migrate-to-v51.ts
@@ -7,7 +7,6 @@ import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics'
 
 import { ElementMigrationData, getElementMigrationData } from '../migrations/data/index.js';
 import { elementMigrationRule } from '../migrations/element-migration/element-migration.js';
-import { iconPathMigrationRule } from '../migrations/icon-path-migration/index.js';
 import { missingTranslateMigrationRule } from '../migrations/ngx-translate/index.js';
 import { splitSizesMigrationRule } from './migrate-split-sizes.js';
 
@@ -19,7 +18,6 @@ export const migrateToV51 = (): Rule => {
     return chain([
       elementMigrationRule(options, migrationData),
       missingTranslateMigrationRule(options),
-      iconPathMigrationRule(options),
       splitSizesMigrationRule(options)
     ])(tree, context);
   };
