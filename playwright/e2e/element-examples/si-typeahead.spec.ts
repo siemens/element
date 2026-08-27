6c3da4e6ae8f058438fb1331b56da569c4b84084
chintankavathia <chintankumar.kavathia@siemens.com>
2026-08-27 12:01:39 +0530

    fix(search-bar): disabled state styles on hover
    prevent disabled state style changes on hover

--- playwright/e2e/element-examples/si-typeahead.spec.ts
+++ playwright/e2e/element-examples/si-typeahead.spec.ts
@@ -15,6 +15,20 @@ test.describe('si-typeahead', () => {
     await si.runVisualAndA11yTests();
   });
 
+  test(`${example} disabled`, async ({ page, si }) => {
+    await si.visitExample(example);
+    await page.getByRole('checkbox', { name: 'Show Icon' }).check();
+    await page.getByRole('checkbox', { name: 'Disable', exact: true }).check();
+
+    const searchBar = page.getByRole('combobox', { name: 'States' });
+    await searchBar.hover();
+    await si.runVisualAndA11yTests('disabled-base-1-hover');
+
+    await page.getByRole('combobox', { name: 'Color Variant' }).selectOption('base-0');
+    await searchBar.hover();
+    await si.runVisualAndA11yTests('disabled-base-0-hover');
+  });
+
   const customExample = 'si-typeahead/si-typeahead-custom';
 
   test(customExample, async ({ page, si }) => {
