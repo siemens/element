354e0c7ff0f54e95a8ebe6f6bb79db4f360dc05e
Bajohr, Rayk <rayk.bajohr@siemens.com>
2026-08-28 14:32:34 +0200

    refactor(auto-collapsable-list): migrate to observeElementSize
    - instead of using ResizeObserverService use a signal to trigger the recalculation

--- projects/element-ng/auto-collapsable-list/si-auto-collapsable-list-measurable.class.ts
+++ projects/element-ng/auto-collapsable-list/si-auto-collapsable-list-measurable.class.ts
@@ -2,32 +2,19 @@
  * Copyright (c) Siemens 2016 - 2026
  * SPDX-License-Identifier: MIT
  */
-import { Directive, ElementRef, inject } from '@angular/core';
-import { ResizeObserverService } from '@siemens/element-ng/resize-observer';
-import { distinctUntilChanged, map, shareReplay } from 'rxjs/operators';
+import { computed, Directive, ElementRef, inject } from '@angular/core';
+import { observeElementSize } from '@siemens/element-ng/resize-observer';
 
+/**
+ * Base class for components/directives that need to measure their own size via ResizeObserver.
+ */
 @Directive()
 export class SiAutoCollapsableListMeasurable {
   protected readonly elementRef = inject(ElementRef<HTMLElement>);
-  private readonly resizeObserverService = inject(ResizeObserverService);
-
+  private readonly resizeEntry = observeElementSize(this.elementRef, { box: 'border-box' });
   /**
-   * Emits on element width changes.
-   *
-   * @defaultValue
-   * ```
-   * this.resizeObserverService
-   *       .observe(this.elementRef.nativeElement, 0, true, true)
-   *       .pipe(
-   *         map(size => size.width),
-   *         distinctUntilChanged(),
-   *         shareReplay(1)
-   *       )
-   * ```
+   * The inline-size (border-box) of the element.
+   * @internal
    */
-  size$ = this.resizeObserverService.observe(this.elementRef.nativeElement, 0, true, true).pipe(
-    map(size => size.width),
-    distinctUntilChanged(),
-    shareReplay(1)
-  );
+  readonly inlineSize = computed(() => this.resizeEntry()?.borderBoxSize[0].inlineSize);
 }
