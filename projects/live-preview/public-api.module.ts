11d0502914a9936cfc535821dfba081ea09780f3
Maximilian Koeller <maximilian.koeller@siemens.com>
2026-09-24 09:01:11 +0200

    refactor(live-preview): cleanup public API
    Currently, the live-preview exposes many components etc. that are intended to be private.
    This changes removes them from the public API surface.
    
    NOTE: Several symbols from the live-preview removed from the public API.
    Those were never intended for public usage, thus this is not a breaking change.

--- projects/live-preview/public-api.module.ts
+++ projects/live-preview/public-api.module.ts
@@ -5,8 +5,6 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { REMOVE_STYLES_ON_COMPONENT_DESTROY } from '@angular/platform-browser';
 
-import { SiExampleOverviewComponent } from './components/si-example-overview/si-example-overview.component';
-import { SiExampleViewerComponent } from './components/si-example-viewer/si-example-viewer.component';
 import {
   SI_LIVE_PREVIEW_CONFIG,
   SI_LIVE_PREVIEW_INTERNALS,
@@ -14,9 +12,7 @@ import {
 } from './interfaces/live-preview-config';
 
 @NgModule({
-  imports: [SiExampleViewerComponent, SiExampleOverviewComponent],
-  providers: [{ provide: REMOVE_STYLES_ON_COMPONENT_DESTROY, useValue: true }],
-  exports: [SiExampleOverviewComponent, SiExampleViewerComponent]
+  providers: [{ provide: REMOVE_STYLES_ON_COMPONENT_DESTROY, useValue: true }]
 })
 export class SiLivePreviewModule {
   static forRoot(
